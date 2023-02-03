7e7a4810f80c0dc809ab587eee66ced435f1d593
Skand Purohit <skandpurohit@gmail.com>
2023-02-02 18:49:07 -0700

    cluster create with dummy values

---
+++ create_eks.tf
@@ -0,0 +1,112 @@
+// Tanzu Mission Control EKS Cluster Type: AWS EKS clusters.
+// Operations supported : Read, Create, Update & Delete
+
+// Read Tanzu Mission Control AWS EKS cluster : fetch cluster details
+data "tanzu-mission-control_ekscluster" "tf_eks_cluster" {
+  credential_name = "sp-eks-tf-cred" // Required
+  region          = "us-east-1"          // Required
+  name            = "sp-tf-auto"        // Required
+}
+
+// Create Tanzu Mission Control AWS EKS cluster entry
+resource "tanzu-mission-control_ekscluster" "tf_eks_cluster" {
+  credential_name = "sp-eks-tf-cred" // Required
+  region          = "us-east-1"          // Required
+  name            = "sp-tf-auto"        // Required
+
+  ready_wait_timeout = "30m" // Wait time for cluster operations to finish (default: 30m).
+
+  meta {
+    description = "description of the cluster"
+    labels      = { "mode" : "automation", "username" : "sp" }
+  }
+
+  spec {
+    cluster_group = "sp-cluster-grp" // Default: default
+    #proxy         = "" //if used 
+
+    config {
+      role_arn           = "arn:aws:iam::687456942232:role/control-plane.11197733977849512268.eks.tmc.cloud.vmware.com" // Required, forces new
+      kubernetes_version = "1.24"                // Required
+      tags               = { "mode" : "terraform" }
+
+      kubernetes_network_config {
+        service_cidr = "10.100.0.0/16" // Forces new
+      }
+
+      logging {
+        api_server         = false
+        audit              = true
+        authenticator      = true
+        controller_manager = false
+        scheduler          = true
+      }
+
+      vpc { // Required
+        enable_private_access = true
+        enable_public_access  = true
+        public_access_cidrs = [
+          "0.0.0.0/0",
+        ]
+        security_groups = [ // Forces new
+          "sg-0395139026e380a43",
+        ]
+        subnet_ids = [ // Forces new
+          "subnet-0071b3b1ec3233eee",
+          "subnet-04c5221ee2db53796",
+          "subnet-0ecfbfb553eef704a",
+          "subnet-09c63bb69bf76578e"
+        ]
+      }
+    }
+
+    nodepool {
+      info {
+        name        = "small-pool" // Required
+        description = "nodepool for eks cluster"
+      }
+
+      spec {
+        // Refer to nodepool's schema
+        role_arn       = "arn:aws:iam::687456942232:role/worker.11197733977849512268.eks.tmc.cloud.vmware.com" // Required
+        ami_type       = "AL2_x86_64"
+        capacity_type  = "ON_DEMAND"
+        root_disk_size = 40 // In GiB, default: 20GiB
+        tags           = { "mode" : "automation" }
+        node_labels    = { "tool" : "tf" }
+
+        subnet_ids = [ // Required
+          "subnet-0071b3b1ec3233eee",
+          "subnet-04c5221ee2db53796",
+          "subnet-0ecfbfb553eef704a",
+          "subnet-09c63bb69bf76578e"
+        ]
+
+        remote_access {
+          ssh_key = "sp-tf-auto-key" // Required (if remote access is specified)
+
+          security_groups = [
+            "sg-0395139026e380a43",
+          ]
+        }
+
+        scaling_config {
+          desired_size = 3
+          max_size     = 5
+          min_size     = 1
+        }
+
+        update_config {
+          max_unavailable_nodes = "10"
+        }
+
+        instance_types = [
+          "t3.medium",
+          "m3.large"
+        ]
+
+      }
+    }
+
+  }
+}
--- eks_creds.tf
+++ eks_creds.tf
@@ -1,31 +1,31 @@
-# Create AWS_EKS credential
-resource "tanzu-mission-control_credential" "aws_eks_cred" {
-  name = "test-sp-cred-terraform"
+# # Create AWS_EKS credential
+# resource "tanzu-mission-control_credential" "aws_eks_cred" {
+#   name = "test-sp-cred-terraform"
 
-  meta {
-    description = "credential"
-    labels = {
-      "automation_type" : "terraform",
-      "user"            : "sp"
-    }
-  }
+#   meta {
+#     description = "credential"
+#     labels = {
+#       "automation_type" : "terraform",
+#       "user"            : "sp"
+#     }
+#   }
 
-  spec {
-    capability = "MANAGED_K8S_PROVIDER"
-    provider = "AWS_EKS"
-    data {
-      aws_credential {
-        account_id = "687456942232"
-        iam_role{
-          arn = "arn:aws:iam::687456942232:role/clusterlifecycle.2273571689724047707.eks.tmc.cloud.vmware.com"
-          ext_id =""
-        }
-      }
-    }
-  }
-}
+#   spec {
+#     capability = "MANAGED_K8S_PROVIDER"
+#     provider = "AWS_EKS"
+#     data {
+#       aws_credential {
+#         account_id = "687456942232"
+#         iam_role{
+#           arn = "arn:aws:iam::687456942232:role/clusterlifecycle.2273571689724047707.eks.tmc.cloud.vmware.com"
+#           ext_id =""
+#         }
+#       }
+#     }
+#   }
+# }
 
 
-data "tanzu-mission-control_credential" "test_cred" {
-  name = "eks-dev-cred-sp"
-}
+# data "tanzu-mission-control_credential" "test_cred" {
+#   name = "eks-dev-cred-sp"
+# }
